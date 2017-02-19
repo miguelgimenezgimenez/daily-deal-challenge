197c3b66968a9e73e9dbd2a22a424b75d3200638
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2017-02-19 07:48:33 +0100

    feat : score

--- imports/ui/Dashboard.jsx
+++ imports/ui/Dashboard.jsx
@@ -8,6 +8,12 @@ import '../../styles/FacesBig.css';
 
 let colors= ['red', 'yellow', 'orange','green'];
 let style;
+let fontColor;
+const auxText={
+  fontSize:40,
+  color:"rgba(0, 0, 0, 0.15)",
+
+}
 class Dashboard extends React.Component {
 
   constructor(){
@@ -20,13 +26,18 @@ class Dashboard extends React.Component {
     const classNames=['sad-big worst-big', 'sad-big', 'regular-big', 'smile-big', 'smile best-big'];
     const rating = Math.floor(this.props.ratings[0].average);
     style = {background:colors[rating], color:colors[rating]};
+    fontColor={color:colors[rating]}
     this.setState({
       className:`circle-big ${classNames[rating]} `
     });
   }
 
-
+renderScore(){
+  if (this.props.ratings) return `${Math.floor((this.props.ratings[0].average/5)*100)}`;
+  return null;
+}
   render () {
+
     return (
       <div>
         <div className="header"
@@ -34,29 +45,31 @@ class Dashboard extends React.Component {
           >
           </div>
 
-          <div className="container">
-            <div className={this.state.className}
-              style={style}
-              >
-
+            <div className="container">
+              <div className="score"
+                style={fontColor}>
+                  {this.renderScore()}
+                  <span style={auxText}>%</span>
+              </div>
+              <div className={this.state.className}
+              style={style}>
+              </div>
             </div>
-
-          </div>
           <div
-            className= "footer"
-            style={style}
-            ></div>
-          </div>
-        )
+              className= "footer"
+              style={style}>
+        </div>
+      </div>
+          )
+        }
       }
-    }
 
 
-    export default createContainer(() => {
-      Meteor.subscribe('ratings');
+      export default createContainer(() => {
+        Meteor.subscribe('ratings');
 
-      return {
-        ratings: Ratings.find({service:'daily'}).fetch(),
+        return {
+          ratings: Ratings.find({service:'daily'}).fetch(),
 
-      };
-    }, Dashboard);
+        };
+      }, Dashboard);
